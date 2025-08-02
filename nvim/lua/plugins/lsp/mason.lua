5c9f58b1f24b4b042eabd6616164a2442d45a254
PeDro0210 <pedroruben021@gmail.com>
2025-08-02 01:39:20 -0600

    changing waybar workspace layout

---
+++ nvim/lua/plugins/lsp/mason.lua
@@ -0,0 +1,20 @@
+return {
+
+  --Haskell LSP
+  require("lspconfig")["hls"].setup({
+    filetypes = { "haskell", "lhaskell", "cabal" },
+  }),
+
+  -- add any tools you want to have installed below
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
+    },
+  },
+}
