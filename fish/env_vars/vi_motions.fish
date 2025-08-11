1a0d57040357d614a7764091f1579c916ad86254
PeDro0210 <pedroruben021@gmail.com>
2025-08-10 22:01:22 -0600

    changing rofi

--- fish/env_vars/vi_motions.fish
+++ fish/env_vars/vi_motions.fish
@@ -2,3 +2,6 @@ set fish_cursor_default block blink
 set fish_cursor_insert line blink
 set fish_cursor_replace_one underscore blink
 set fish_cursor_visual block
+
+bind -M visual y fish_clipboard_copy
+bind p fish_clipboard_paste
