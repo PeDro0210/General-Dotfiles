7d7d60c72d420d8a7256bad76bf820854d542659
PeDro0210 <pedroruben021@gmail.com>
2025-09-04 15:15:19 -0600

    feature: generic implemetion for adding lsp support

--- nvim/lua/plugins/lsp/general.lua
+++ nvim/lua/plugins/lsp/general.lua
@@ -1,30 +1,47 @@
+local lsps_with_file_types = {
+	nil_ls = { "nix" },
+	nixd = { "nix" },
+	clangd = { "c", "cpp", "cc" },
+	pyright = { "py" },
+	zls = { "zig" },
+}
+
+local function format_lsp(lsp_name, file_types)
+	return {
+
+		{
+			"neovim/nvim-lspconfig",
+			name = "lspconfig." .. lsp_name,
+			ft = file_types,
+			opts = {},
+			config = function(_, opts)
+				require("lspconfig").lsp_name.setup(opts)
+			end,
+		},
+
+		vim.lsp.enable(lsp_name),
+	}
+end
+
+local function inyect_lsps_args_into_formatter()
+	local formatted_lsps = {}
+
+	for lsp, file_types in pairs(lsps_with_file_types) do
+		table.insert(formatted_lsps, format_lsp(lsp, file_types)) -- I can't really handle this indexing
+	end
+
+	return formatted_lsps
+end
+
 return {
 
 	vim.lsp.enable("lua_ls"),
 
-	-- all for nix related
-	{
-		"neovim/nvim-lspconfig",
-		name = "lspconfig.nil_ls",
-		ft = { "nix" },
-		opts = {},
-		config = function(_, opts)
-			require("lspconfig").nil_ls.setup(opts)
-		end,
-	},
-	vim.lsp.enable("nil_ls"),
-	{
-		"neovim/nvim-lspconfig",
-		name = "lspconfig.nixd",
-		ft = { "nix" },
-		opts = {},
-		config = function(_, opts)
-			require("lspconfig").nixd.setup(opts)
-		end,
-	},
-	vim.lsp.enable("nixd"),
+	inyect_lsps_args_into_formatter(),
+
 	{
 		"stevearc/conform.nvim",
+
 		optional = true,
 		opts = {
 			formatters_by_ft = {
@@ -32,38 +49,4 @@ return {
 			},
 		},
 	},
-	{
-		"neovim/nvim-lspconfig",
-		name = "lspconfig.clangd",
-		ft = { "c", "cpp" },
-		opts = {},
-		config = function(_, opts)
-			require("lspconfig").clangd.setup(opts)
-		end,
-	},
-	vim.lsp.enable("clangd"),
-
-	-- this will just work for pyright with nix-shell, cause I ain't installing pyright, nor the interpreter
-
-	{
-		"neovim/nvim-lspconfig",
-		name = "lspconfig.pyright",
-		ft = { "py" },
-		config = function(_, opts)
-			require("lspconfig").pyright.setup(opts)
-		end,
-	},
-
-	vim.lsp.enable("pyright"),
-
-	{
-		"neovim/nvim-lspconfig",
-		name = "lspconfig.zls",
-		ft = { "zig" },
-		config = function(_, opts)
-			require("lspconfig").zls.setup(opts)
-		end,
-	},
-
-	vim.lsp.enable("zls"),
 }
