0061c14b09bb2702d93c2e0b1d0869128592061a
PeDro0210 <pedroruben021@gmail.com>
2026-01-14 15:29:26 -0600

    module/nvim: adding own lualine

---
+++ nvim/lua/config/lua_line.lua
@@ -0,0 +1,31 @@
+local config = {
+	options = {
+		-- Disable sections and component separators
+		component_separators = "",
+		section_separators = "",
+		theme = "auto",
+	},
+	sections = {
+		-- these are to remove the defaults
+		lualine_a = {
+			"mode",
+		},
+		lualine_b = { "branch" },
+		lualine_c = { "lsp_status" },
+		lualine_y = { "progress" },
+		-- These will be filled later
+		lualine_x = { "buffers" },
+	},
+	inactive_sections = {
+		-- these are to remove the defaults
+		lualine_a = {
+			"location",
+		},
+		lualine_b = {},
+		lualine_y = {},
+		lualine_z = {},
+		lualine_c = {},
+		lualine_x = {},
+	},
+}
+require("lualine").setup(config)
